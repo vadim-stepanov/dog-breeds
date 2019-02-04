ee71ae991acef5155c8e89ab6b385a6fa2f1589d
Vadim <kometa75@gmail.com>
2019-02-04 12:34:01 +0200

    Updates

---
+++ precache-manifest.a8dd62cd48568142720bd99927078334.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "29ddd8193187b11efaf1b92fa12ea36d",
+    "url": "/dog-breeds/static/media/dogtime.29ddd819.png"
+  },
+  {
+    "revision": "90099f46820ae1ef0155",
+    "url": "/dog-breeds/static/js/main.90099f46.chunk.js"
+  },
+  {
+    "revision": "816ece26351e03b48ac8300a80a4f3d0",
+    "url": "/dog-breeds/static/media/404-puppy.816ece26.png"
+  },
+  {
+    "revision": "df479c19d2888a7a5320",
+    "url": "/dog-breeds/static/js/runtime~main.df479c19.js"
+  },
+  {
+    "revision": "8cd04ba5e14dd07a9612945a6acddfcd",
+    "url": "/dog-breeds/static/media/background-home.8cd04ba5.jpg"
+  },
+  {
+    "revision": "57a85a657de760ac5a5467f2650f4ba1",
+    "url": "/dog-breeds/static/media/american-kennel-club.57a85a65.png"
+  },
+  {
+    "revision": "0d35df89233dc69b03e3978ef8464cb7",
+    "url": "/dog-breeds/static/media/loader-puppy.0d35df89.png"
+  },
+  {
+    "revision": "4a76b5ab9342bb32bb12ea0cc277180a",
+    "url": "/dog-breeds/static/media/petwave.4a76b5ab.png"
+  },
+  {
+    "revision": "074a36cf93eb59084bad25c879bbf309",
+    "url": "/dog-breeds/static/media/purina.074a36cf.png"
+  },
+  {
+    "revision": "e7e8ec8ab73ab9d0d3c8",
+    "url": "/dog-breeds/static/js/1.e7e8ec8a.chunk.js"
+  },
+  {
+    "revision": "e7e8ec8ab73ab9d0d3c8",
+    "url": "/dog-breeds/static/css/1.44d57ce4.chunk.css"
+  },
+  {
+    "revision": "f1aea3bbd0aa345d87d93f565dc95216",
+    "url": "/dog-breeds/index.html"
+  }
+];
